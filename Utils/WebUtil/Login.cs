82dca31f1442a42e6f05945d995baa6813bb15ac
Rodrigo Machado <rodrigoborgesmachado@gmail.com>
2023-06-27 12:03:03 -0300

    Retirando linhas desnecessárias.

--- Utils/WebUtil/Login.cs
+++ Utils/WebUtil/Login.cs
@@ -1,12 +1,8 @@
 ﻿using JSON;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Util.WebUtil
