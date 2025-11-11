00d2d145cab1e91eeaea2b29c96280bb4ba16e4e
Rodrigo Machado <rodrigo.m@condocontrol.com>
2025-11-11 10:38:25 -0300

    INcluindo arquivos.

---
+++ CrudForms/FrontEndComponents/Icons/HomeIcon.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const HomeIcon = ({ size = 21, color = '#1B4332' }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M8 0L0 6V8H1V15H4V10H7V15H15V8H16V6L14 4.5V1H11V2.25L8 0ZM9 10H12V13H9V10Z"
+      fill={color}
+    />
+  </svg>
+);
+
+export default HomeIcon;
